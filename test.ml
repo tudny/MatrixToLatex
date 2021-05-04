746ae8c64e828515dcd1ee0a8cf6bd90f95b33ec
tudny <aleksanderwt@gmail.com>
2021-05-04 16:05:44 +0200

    added demo

--- test.ml
+++ test.ml
@@ -1,15 +1,33 @@
+(* 
+let y1 = Stair.of_file "matrix1.in";;
+let a = Stair.of_file "matrix2.in";;
+let x2 = Matrix.transpose @@ Stair.of_file "matrix3.in";;
 
-open Rational
-open Rational_matrix
+let rational_mult = Matrix.mult (Rational.sum) (Rational.mult) (Rational.of_int 0);;
+let print = Matrix.print_matrix (Rational.to_string);;
 
-let x = Rational_matrix.make 3 4 (Rational.make 3 4);;
-let y = Rational_matrix.make 4 5 (Rational.make 2 10);;
-let z = x *! y;;
+print y1;;
+print a;;
+print x2;;
 
-Rational_matrix.print_matrix x;;
-Rational_matrix.print_matrix y;;
-print_matrix z;;
+let print_size m =
+  let (x, y) = Matrix.size m in 
+  Printf.printf "%d %d\n" x y
+;;
+(* 
+print_size y1;;
+print_size a;;
+print_size x2;; *)
 
-let a = of_file "example.matrix";;
-print_matrix a;;
+let t1 =  rational_mult y1 a;;
+let t2 = rational_mult t1 x2;;
+
+Matrix.print_matrix (Rational.to_string) t2;; *)
+
+let rational_mult = Matrix.mult (Rational.sum) (Rational.mult) (Rational.of_int 0);;
+let print = Matrix.print_matrix (Rational.to_string);;
+
+let m = Stair.of_file "example.matrix";;
+
+let d = Stair.stair m (Matrix.to_latex (Rational.to_latex));;
 
