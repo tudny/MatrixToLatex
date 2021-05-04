bce363c27046417d6d426a53ef9bf2ab2d2b3c38
tudny <aleksanderwt@gmail.com>
2021-05-04 16:11:24 +0200

    renewed demo

--- test.ml
+++ test.ml
@@ -1,33 +1,8 @@
-(* 
-let y1 = Stair.of_file "matrix1.in";;
-let a = Stair.of_file "matrix2.in";;
-let x2 = Matrix.transpose @@ Stair.of_file "matrix3.in";;
-
-let rational_mult = Matrix.mult (Rational.sum) (Rational.mult) (Rational.of_int 0);;
-let print = Matrix.print_matrix (Rational.to_string);;
-
-print y1;;
-print a;;
-print x2;;
-
-let print_size m =
-  let (x, y) = Matrix.size m in 
-  Printf.printf "%d %d\n" x y
-;;
-(* 
-print_size y1;;
-print_size a;;
-print_size x2;; *)
-
-let t1 =  rational_mult y1 a;;
-let t2 = rational_mult t1 x2;;
-
-Matrix.print_matrix (Rational.to_string) t2;; *)
-
-let rational_mult = Matrix.mult (Rational.sum) (Rational.mult) (Rational.of_int 0);;
-let print = Matrix.print_matrix (Rational.to_string);;
-
+(* wczytanie macierzy [m] z pliku [example.matrix] *)
 let m = Stair.of_file "example.matrix";;
 
+(* zeschodkowanie macierzy [m] oraz wypisanie kroków w postaci LaTeX'a *)
 let d = Stair.stair m (Matrix.to_latex (Rational.to_latex));;
 
+(* zeschodkowanie macierzy [m] oraz wypisanie kroków w postaci czytelnej *)
+let d = Stair.stair m (Matrix.print_matrix (Rational.to_string));;
